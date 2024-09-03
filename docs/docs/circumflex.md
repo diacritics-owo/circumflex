af436dd5b66ecd655e43a8687a5fe34bc96ef75c
diacritics-owo <176600453+diacritics-owo@users.noreply.github.com>
2024-09-03 19:37:58 +0530

    set up docusaurus

---
+++ docs/docs/circumflex.md
@@ -0,0 +1,13 @@
+---
+sidebar_position: 0
+slug: /
+---
+
+# Circumflex
+
+A Fabric/Quilt library mod
+
+## Features
+
+- Registry initializer API (inspired by [owo-lib](https://github.com/wisp-forest/owo-lib))
+- Command registration API
---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,75 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Circumflex',
+  tagline: 'A Fabric/Quilt library mod',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://diacritics-owo.github.io/',
+  baseUrl: '/',
+
+  organizationName: 'diacritics-owo',
+  projectName: 'circumflex',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          editUrl:
+            'https://github.com/diacritics-owo/circumflex/tree/main/docs/',
+          routeBasePath: '/',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    navbar: {
+      title: 'Circumflex',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'documentation',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          href: 'https://github.com/diacritics-owo/circumflex',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      links: [
+        {
+          label: 'GitHub',
+          href: 'https://github.com/diacritics-owo/circumflex',
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} diacritics-owo`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
