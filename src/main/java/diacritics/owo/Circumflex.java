37df35db3fc8cde366101343098e758f78a6111d
diacritics-owo <176600453+diacritics-owo@users.noreply.github.com>
2024-09-03 10:39:24 +0530

    initial commit

---
+++ src/main/java/diacritics/owo/Circumflex.java
@@ -0,0 +1,13 @@
+package diacritics.owo;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Circumflex implements ModInitializer {
+	public static final String MOD_ID = "circumflex";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {}
+}
